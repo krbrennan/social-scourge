23e7931d25bdf206fda02b3da41dc8d4398246d6
krbrennan <krb.brennan@gmail.com>
2020-07-01 21:14:03 -0400

    added signup authentication

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,31 @@
 const functions = require('firebase-functions');
 
+const config = {
+    apiKey: "AIzaSyDBgARBXtewBiPTLYQDytvP_iNeo76vEO0",
+    authDomain: "social-scourge.firebaseapp.com",
+    databaseURL: "https://social-scourge.firebaseio.com",
+    projectId: "social-scourge",
+    storageBucket: "social-scourge.appspot.com",
+    messagingSenderId: "444991870869",
+    appId: "1:444991870869:web:cf2ff6edfa7c1e25cb089c",
+    measurementId: "G-JDFQE5846E"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
 var admin = require("firebase-admin");
+const app = require('express')();
+admin.initializeApp({
+    credential: admin.credential.cert(require('./key/admin.json')),
+    databaseURL: "https://social-scourge.firebaseio.com"
+});
+const db = admin.firestore();
 
 var serviceAccount = require("./key/admin.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(require('./key/admin.json')),
-  databaseURL: "https://social-scourge.firebaseio.com"
-});
-
-const express = require('express');
-const app = express();
 
 app.get('/posts', (req, res) => {
-    admin
-    .firestore()
+    db
     .collection('posts')
     // order posts by date created
     .orderBy('created', 'desc')
@@ -42,8 +53,7 @@ app.post('/post', (req, res) => {
         username: req.body.username,
         created: new Date().toISOString()
     }
-    admin
-    .firestore()
+    db
     .collection('posts')
     .add(newPost)
     .then((doc) => {
@@ -55,5 +65,52 @@ app.post('/post', (req, res) => {
     })
 })
 
+// signup route
+
+app.post('/signup', (req, res) => {
+    const newUser = {
+        "email": req.body.email,
+        "password": req.body.password,
+        "confirmPassword": req.body.password,
+        "username": req.body.username,
+    };
+
+    // validate user later
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then((token) => {
+            res.status(201).json({ token })
+        })
+        .catch((err) => {
+            let errCode = err.code;
+            let errMsg = err.message;
+            if(errCode == 'auth/email-already-in-use') {
+                console.log('This email is already associated with an account')
+            } else if(errCode == 'auth/weak-password') {
+                console.log('Your password is too weak.')
+            } else if (errCode == 'auth/invalid-email'){
+                console.log('This is not a valid email')
+            } else {
+                console.log(errMsg)
+            } 
+            res.status(500).json({error: err.code})
+        })
+})
+
+app.post('/signin', (req, res) => {
+    const credentials = {
+        "username": req.body.username,
+        "password": req.body.password
+    }
+    firebase.auth().signInWithEmailAndPassword(credentials.username, credentials.password)
+        .then(() => {
+            res.status(200).json({message: "logged in successfully"})
+        })
+        .catch((err) => {
+            res.status(500).json({error: err.code})
+        })
+})
 
 exports.api = functions.https.onRequest(app);
