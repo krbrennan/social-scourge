2f0010835b2d94a6b0b6474ca476f8669c592cc1
krbrennan <krb.brennan@gmail.com>
2020-07-22 20:50:28 -0400

    getPosts on home page working

--- socialscourge-client/src/pages/home.js
+++ socialscourge-client/src/pages/home.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import axios from 'axios';
 
 // Material-UI
 import Grid from '@material-ui/core/Grid';
 
 class home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: null
+        }
+    }
+
+    componentDidMount(){
+        axios.get('/posts')
+            .then((postData) => {
+                // console.log(data)
+                this.setState({
+                    posts: postData.data
+                })
+            })
+            // .then(() => {
+            //     return res.status(200)
+            // })
+            // .catch((err) => {
+            //     res.status(500).json(err)
+            // })
+    }
+
     render() {
         return (
-            <Grid container className='container' spacing={16}>
+            <Grid container className='container'>
                 <Grid item sm={8} xs={12}>
-                    <p>Content</p>
+                    { this.state.posts ? 
+                        // console.log(this.state.posts)
+                        this.state.posts.map((postItem) => {
+                           return <p>{postItem.body}</p> 
+                        })
+                    :
+                        <p>LOADING...</p>
+                    }
                 </Grid>
                 <Grid item sm={4} xs={12}>
                     <p>Profile</p>
