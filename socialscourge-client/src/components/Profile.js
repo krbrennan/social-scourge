cfa343c9b7bc6bf1c7b8d5fcebd53982e8cc9c9a
krbrennan <krb.brennan@gmail.com>
2020-08-24 18:30:44 -0400

    first commit from new computer/environment

--- socialscourge-client/src/components/Profile.js
+++ socialscourge-client/src/components/Profile.js
@@ -1,70 +1,100 @@
-import React, { Component } from 'react';
-import Link from 'react-router-dom/Link';
+import React, { Component } from "react";
+import Link from "react-router-dom/Link";
 
 // MUI
-import withStyles from '@material-ui/core/styles/withStyles';
+import withStyles from "@material-ui/core/styles/withStyles";
+import MuiLink from "@material-ui/core/Link";
 
-import axios from 'axios';
-import { Typography } from '@material-ui/core';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
+import axios from "axios";
+import { Typography } from "@material-ui/core";
+
+// icons
 const styles = {
-    loggedInDiv: {
-        height: 400,
-        width: 400,
-        background: 'yellow',
-        margin: '0'
-    },
-}
+  loggedInDiv: {
+    height: 400,
+    width: 400,
+    background: "yellow",
+    margin: "0",
+  },
+};
 
 class Profile extends Component {
-
-    componentDidMount(){
-       if(localStorage.getItem('FBIdToken')){
-           this.setState({
-               isLoggedIn: true
-           })
-       } else{
-            this.setState({
-                isLoggedIn: false
-            })
-       }
+  componentDidMount() {
+    if (localStorage.getItem("FBIdToken")) {
+      this.setState({
+        isLoggedIn: true,
+      });
+    } else {
+      this.setState({
+        isLoggedIn: false,
+      });
     }
-    
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            isLoggedIn: false,
-            imgUrl: '',
-            username: '',
-            bio: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      imgUrl: "",
+      username: "",
+      bio: "",
+    };
+  }
 
-    render(){
-        const { classes } = this.props;
-        return(
-            <div>
-                { this.state.isLoggedIn ?
-                    <div>
-                        <div className={classes.loggedInDiv}>
-                            <img />
-                        </div>
-                    </div>
-                :
-                    <div>
-                        <div>
+  render() {
+    const {
+      classes,
+      user: {
+        credentials: { username, createdAt, imageUrl, bio, website, location },
+        loading,
+      },
+    } = this.props;
 
-                        </div>
-                    </div>
-                }
-            </div>
-        )
-    }
+    // let profileMarkup = !loading ? (
+    //   authenticated ? (
+    //     <Paper className={classes.paper}>
+    //       <div className={classes.profile}>
+    //         <div className="profile-image">
+    //           <img src={imageUrl} alt="profile" />
+    //         </div>
+    //         <hr />
+    //         <div className="profile-details">
+    //           <MuiLink
+    //             component={Link}
+    //             to={`/users/${username}`}
+    //             color="primary"
+    //             variant="h5"
+    //           >
+    //             @{username}
+    //           </MuiLink>
+    //           <hr />
+    //           {bio && <Typography variant="body2">{bio}</Typography>}
+    //           <hr />
+    //         </div>
+    //       </div>
+    //     </Paper>
+    //   ) : null
+    // ) : (
+    //   <p>Loading</p>
+    // );
+    return <h1>Anus</h1>;
+    // return profileMarkup;
+  }
 }
 
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
 
-export default withStyles(styles)(Profile);
+Profile.propTypes = {
+  user: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps)(withStyles(styles)(Profile));
 
 // if localStorage.getItem('FBIdToken')
 // show profile information
--- socialscourge-client/src/redux/actions/userActions.js
+++ socialscourge-client/src/redux/actions/userActions.js
@@ -1,63 +1,86 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from '../reducers/types';
-import axios from 'axios';
+import {
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  LOADING_USER,
+  SET_UNAUTHENTICATED,
+} from "../reducers/types";
+import axios from "axios";
 
 export const loginUser = (userData, history) => (dispatch) => {
-    dispatch({ type: LOADING_UI});
-     // send req to server and show errors, if successful then show errors
-     axios.post('/signin', userData)
-        .then((postData) => {
-            // console.log(postData.data)
-            const FBIdToken = `Bearer ${postData.data.token}`
-            localStorage.setItem('FBIdToken', `Bearer ${postData.data.token}`)
-            axios.defaults.headers.common['Authorization'] = FBIdToken
-            dispatch(getUserData());
-            dispatch({ type: CLEAR_ERRORS});
-            history.push('/')
-        })
-        .catch((err) => {
-            dispatch({
-                type: SET_ERRORS,
-                payload: err.response.data.message
-            })
-        })
-}
+  dispatch({
+    type: LOADING_UI
+  });
+  // send req to server and show errors, if successful then show errors
+  axios
+    .post("/signin", userData)
+    .then((postData) => {
+      // console.log(postData.data)
+      const FBIdToken = `Bearer ${postData.data.token}`;
+      localStorage.setItem("FBIdToken", `Bearer ${postData.data.token}`);
+      axios.defaults.headers.common["Authorization"] = FBIdToken;
+      dispatch(getUserData());
+      dispatch({
+        type: CLEAR_ERRORS
+      });
+      history.push("/");
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data.message,
+      });
+    });
+};
 
 export const signupUser = (newUserData, history) => (dispatch) => {
-    dispatch({ type: LOADING_UI});
-     // send req to server and show errors, if successful then show errors
-     axios.post('/signup', newUserData)
-        .then((postData) => {
-            // console.log(postData.data)
-            const FBIdToken = `Bearer ${postData.data.token}`
-            localStorage.setItem('FBIdToken', `Bearer ${postData.data.token}`)
-            axios.defaults.headers.common['Authorization'] = FBIdToken
-            dispatch(getUserData());
-            dispatch({ type: CLEAR_ERRORS});
-            history.push('/')
-        })
-        .catch((err) => {
-            dispatch({
-                type: SET_ERRORS,
-                payload: err.response.data.message
-            })
-        })
-}
+  dispatch({
+    type: LOADING_UI
+  });
+  // send req to server and show errors, if successful then show errors
+  axios
+    .post("/signup", newUserData)
+    .then((postData) => {
+      // console.log(postData.data)
+      const FBIdToken = `Bearer ${postData.data.token}`;
+      localStorage.setItem("FBIdToken", `Bearer ${postData.data.token}`);
+      axios.defaults.headers.common["Authorization"] = FBIdToken;
+      dispatch(getUserData());
+      dispatch({
+        type: CLEAR_ERRORS
+      });
+      history.push("/");
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data.message,
+      });
+    });
+};
 
 export const logoutUser = () => (dispatch) => {
-    localStorage.removeItem('FBIdToken');
-    delete axios.defaults.headers.common['Authorization'];
-    dispatch({ type: SET_UNAUTHENTICATED});
-}
+  localStorage.removeItem("FBIdToken");
+  delete axios.defaults.headers.common["Authorization"];
+  dispatch({
+    type: SET_UNAUTHENTICATED
+  });
+};
 
 export const getUserData = () => (dispatch) => {
-    axios.get('/user')
-        .then((res) => {
-            dispatch({
-                type: SET_USER,
-                payload: res._data
-            })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-}
+  dispatch({
+    type: LOADING_USER
+  });
+  axios
+    .get("/user")
+    .then((res) => {
+      dispatch({
+        type: SET_USER,
+        payload: res._data,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
